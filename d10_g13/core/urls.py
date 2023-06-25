a71775f8617b77a1825045834df5ecae7d1ed61c
Lucas Gabriel Gómez <lucasgabrielg2001@gmail.com>
2023-06-25 01:22:26 -0300

    Creando mas templates

--- d10_g13/core/urls.py
+++ d10_g13/core/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('index/', views.indexView),
+    path('index/', views.index_view, name='index'),
+    path('publicaciones/', views.publicaciones_view, name='publicaciones'),
+    path('contactos/', views.contactos_view, name='contactos'),
+    path('registrarse/', views.registrarse_view, name='registrarse'),
+    path('iniciar-sesion/', views.iniciar_sesion_view, name='iniciar-sesion'),
+    path('cerrar-sesion/', views.cerrar_sesion_view, name='cerrar-sesion'),
 ]
--- d10_g13/core/views.py
+++ d10_g13/core/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def indexView(request):
+def index_view(request):
     return render(request, 'index.html', {})
+
+def publicaciones_view(request):
+    return render(request, 'publicaciones.html', {})
+
+def contactos_view(request):
+    return render(request, 'contactos.html', {})
+
+def registrarse_view(request):
+    return render(request, 'registrarse.html', {})
+
+def iniciar_sesion_view(request):
+    return render(request, 'iniciar-sesion.html', {})
+
+def cerrar_sesion_view(request):
+    return render(request, 'cerrar-sesion.html', {})
